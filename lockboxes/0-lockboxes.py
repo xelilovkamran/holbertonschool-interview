9b32252b17667fd2d60115b98cd1fb003dfd97e4
xelilovkamran <xlilovkamran@gmail.com>
2024-02-29 16:30:40 +0400

    PEP8 validation

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -5,6 +5,7 @@
 You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 '''
 
+
 def canUnlockAll(boxes):
     unlockedBoxes = set()
     for i, element in enumerate(boxes):
@@ -12,8 +13,8 @@ def canUnlockAll(boxes):
             if box != i and box < len(boxes):
                 unlockedBoxes.add(box)
     unlockedBoxes.add(0)
-    
+
     for index, indexedBox in enumerate(unlockedBoxes):
         if index != indexedBox and index != 0:
             return False
-    return True        
+    return True
