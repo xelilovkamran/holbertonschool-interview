9a1400e22f1f5b6f2602e2a334b0f2eeaaf8da5f
xelilovkamran <xlilovkamran@gmail.com>
2024-02-29 16:45:54 +0400

    documented

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+''' 
+You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+'''
+
+
 def canUnlockAll(boxes):
     ''' 
     You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
