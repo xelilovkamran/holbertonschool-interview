dff2d1dcecf8da82ea461f5c0abe91759400d3ec
xelilovkamran <xlilovkamran@gmail.com>
2024-02-29 23:15:00 +0400

    PEP8 validation

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 """
-You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1 and,
+each box may contain keys to the other boxes.
 """
 
 
