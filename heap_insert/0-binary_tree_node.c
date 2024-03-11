02311377c4a2b04d2115d32d08907605dc832e70
xelilovkamran <xlilovkamran@gmail.com>
2024-03-11 10:08:32 +0400

    new node

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -5,8 +5,17 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
     binary_tree_t *new;
     new = malloc(sizeof(binary_tree_t));
+    if (new == NULL) {
+        return (NULL);
+    }
     new->n = value;
-    new->parent = parent;
+
+    if (parent != NULL) {
+        new->parent = parent;
+    }
+    else {
+        new->parent = new;
+    }
 
     return (new);
 }
