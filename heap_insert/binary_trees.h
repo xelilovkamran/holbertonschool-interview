03b74656ec07e313854f926857b3789b7e016d90
xelilovkamran <xlilovkamran@gmail.com>
2024-03-12 10:44:55 +0400

    new node

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -19,8 +19,6 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
-
 void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
