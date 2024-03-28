2b995104a6a2d1849f517a80de1574516238d257
xelilovkamran <xlilovkamran@gmail.com>
2024-03-29 00:16:20 +0400

    sandpiles

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -46,31 +46,30 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 int i, j;
 for (i = 0; i < 3; i++)
-{
 for (j = 0; j < 3; j++)
 {
 grid1[i][j] += grid2[i][j];
 grid2[i][j] = 0;
 }
-}
-if (stable(grid1)) return;
+if (stable(grid1))
+return;
 while (!stable(grid1))
 {
 printf("=\n");
 print_grid(grid1);
 for (i = 0; i < 3; i++)
-{
 for (j = 0; j < 3; j++)
-{
 if (grid1[i][j] > 3)
 {
 grid2[i][j] -= 4;
-if (i - 1 >= 0) grid2[i - 1][j] += 1;
-if (i + 1 < 3) grid2[i + 1][j] += 1;
-if (j - 1 >= 0) grid2[i][j - 1] += 1;
-if (j + 1 < 3) grid2[i][j + 1] += 1;
-}
-}
+if (i - 1 >= 0)
+grid2[i - 1][j] += 1;
+if (i + 1 < 3)
+grid2[i + 1][j] += 1;
+if (j - 1 >= 0)
+grid2[i][j - 1] += 1;
+if (j + 1 < 3)
+grid2[i][j + 1] += 1;
 }
 for (i = 0; i < 3; i++)
 {
