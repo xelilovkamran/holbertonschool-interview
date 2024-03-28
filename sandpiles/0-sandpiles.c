654ed0dfdb9c28772e2da73f2d87212121e0cabc
xelilovkamran <xlilovkamran@gmail.com>
2024-03-28 23:46:56 +0400

    sandpiles

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -22,6 +22,22 @@ int stable(int grid[3][3]) {
  *
  */
 
+static void print_grid(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]) {
     int i, j;
     for (i = 0; i < 3; i++) {
