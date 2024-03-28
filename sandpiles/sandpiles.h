9eb8520b83ee1338c05f92c2f73b767d741d9581
xelilovkamran <xlilovkamran@gmail.com>
2024-03-28 16:06:09 +0400

    sandpiles

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -9,6 +9,5 @@ static void print_grid(int grid[3][3]);
 
 int stable(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid(int grid[3][3]);
 
 #endif /* _SANDPILES_H_ */
