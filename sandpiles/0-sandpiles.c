860bcd36b5b519fd918ad412b92c80f3e8b731f5
xelilovkamran <xlilovkamran@gmail.com>
2024-03-28 23:50:25 +0400

    sandpiles

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -38,6 +38,16 @@ static void print_grid(int grid[3][3])
     }
 }
 
+// write description for sandpiles_sum
+
+/**
+ * sandpiles_sum - Computes the sum of two sandpiles
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ * 
+ * Return: void
+*/
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]) {
     int i, j;
     for (i = 0; i < 3; i++) {
@@ -71,7 +81,6 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3]) {
 
                     if (j + 1 < 3)
                         grid2[i][j + 1] += 1;
-
                 }
             }
         }
