41b0acf31e844322f77981b1be8bead538259cff
xelilovkamran <xlilovkamran@gmail.com>
2024-03-28 23:59:59 +0400

    sandpiles

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -7,13 +7,13 @@
  * Return: Is sandpile stable or not(1,0)
  */
 
-int stable(int grid[3][3]) 
+int stable(int grid[3][3])
 {
 int i, j;
-for (i = 0; i < 3; i++)
-    for (j = 0; j < 3; j++)
-        if (grid[i][j] > 3)
-            return 0;
+for(i = 0; i < 3; i++)
+for(j = 0; j < 3; j++)
+if(grid[i][j] > 3)
+return 0;
 return 1;
 }
 
@@ -27,15 +27,15 @@ static void print_grid(int grid[3][3])
 {
 int i, j;
 
-for (i = 0; i < 3; i++)
+for(i = 0; i < 3; i++)
 {
-    for (j = 0; j < 3; j++)
-    {
-        if (j)
-            printf(" ");
-        printf("%d", grid[i][j]);
-    }
-    printf("\n");
+for(j = 0; j < 3; j++)
+{
+if(j)
+printf(" ");
+printf("%d", grid[i][j]);
+}
+printf("\n");
 }
 }
 
@@ -50,15 +50,15 @@ for (i = 0; i < 3; i++)
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]) 
 {
 int i, j;
-for (i = 0; i < 3; i++) {
-    for (j = 0; j < 3; j++) {
-        grid1[i][j] += grid2[i][j];
-        grid2[i][j] = 0;
-    }
+for(i = 0; i < 3; i++) {
+for(j = 0; j < 3; j++) {
+grid1[i][j] += grid2[i][j];
+grid2[i][j] = 0;
+}
 }
 
 if (stable(grid1))
-    return;
+return;
 
 while (!stable(grid1)) {
     printf("=\n");
