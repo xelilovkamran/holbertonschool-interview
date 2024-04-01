15d021cd69df2ac86edb1c58583f88bdaeb7fbac
xelilovkamran <xlilovkamran@gmail.com>
2024-04-01 10:31:19 +0400

    Linked list palindrome

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -29,10 +29,13 @@ int is_palindrome(listint_t **head) {
     for (i = 0, j = n - 1; i < n / 2; i++, j--) {
         if (array[i] != array[j]) {
             free(array);
+            free_listint(*head);
             return (0);
         }
     }
 
     free(array);
+    free_listint(*head);
+
     return (1);
 }
