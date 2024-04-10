c2a02dc23e8923494a3ae871944af0cd0a32c5fb
xelilovkamran <xlilovkamran@gmail.com>
2024-04-10 19:32:31 +0400

    log parsing

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -6,9 +6,9 @@ all info will be printed"""
 
 import sys
 
-file_size = 0
-count = 0
-ids = {
+logs = []
+total_size = 0
+filtering_by_status = {
     "200": 0,
     "301": 0,
     "400": 0,
@@ -20,26 +20,23 @@ ids = {
 }
 
 
-def print_msg(ids, file_size):
-    print("File size: {}".format(file_size))
-    for key, val in sorted(ids.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+def print_msg():
+    """Prints the stats"""
+    print(f"File size: {total_size}")
+    for element in filtering_by_status.items():
+        if element[1] != 0:
+            print(f"{element[0]}: {element[1]}")
 
 
 try:
     for line in sys.stdin:
-        nums = line.rstrip().split(' ')
-        try:
-            if nums[-2] in ids:
-                ids[nums[-2]] += 1
-            file_size += int(nums[-1])
-            count += 1
-            if count % 10 == 0:
-                print_msg(ids, file_size)
-        except BaseException:
-            pass
+        line = line.strip()
+        logs.append(line)
+        line = line.split(" ")
+        filtering_by_status[line[-2]] += 1
+        total_size += int(line[-1])
+        if len(logs) % 10 == 0 and len(logs) != 0:
+            print_msg()
 
-
-finally:
-    print_msg(ids, file_size)
+except KeyboardInterrupt:
+    print_msg()
