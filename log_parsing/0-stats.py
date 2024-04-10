82e69c13e5cfc79979f4a71ae5e4d30f025a240f
xelilovkamran <xlilovkamran@gmail.com>
2024-04-10 21:35:40 +0400

    log parse

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -39,7 +39,7 @@ try:
         new_line = line.rstrip().split(" ")
         if len(new_line) == 9 or len(new_line) == 7:
             try:
-                new_line = new_line.split(" ")
+                # new_line = new_line.split(" ")
                 logs += 1
                 status_codes[new_line[-2]] += 1
                 total_size += int(new_line[-1])
