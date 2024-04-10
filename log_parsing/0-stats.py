7b1951845cc81a2c30375a2ee780441c220b0df8
xelilovkamran <xlilovkamran@gmail.com>
2024-04-10 20:34:44 +0400

    log parse

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""Reads stdin line by line and computes metrics
-If count of lines is evenly divided by 10 and/or
-keyboardinterrupt
-all info will be printed"""
+"""Input stats"""
 
 import sys
 
-logs = []
+logs = 0
 total_size = 0
 filtering_by_status = {
     "200": 0,
@@ -20,8 +17,8 @@ filtering_by_status = {
 }
 
 
-def print_msg(statues, total_size):
-    print(f"File size: {total_size}")
+def print_msg(statues, total):
+    print(f"File size: {total}")
     for element in statues.items():
         if element[1] != 0:
             print(f"{element[0]}: {element[1]}")
@@ -29,13 +26,12 @@ def print_msg(statues, total_size):
 
 try:
     for line in sys.stdin:
-        new_line = line.rstrip()
+        new_line = line.rstrip().split(" ")
         try:
-            logs.append(new_line)
-            new_line = new_line.split(" ")
+            logs += 1
             filtering_by_status[new_line[-2]] += 1
             total_size += int(new_line[-1])
-            if len(logs) % 10 == 0 and len(logs) != 0:
+            if logs % 10 == 0 and logs != 0:
                 print_msg(filtering_by_status, total_size)
         except BaseException:
             pass
