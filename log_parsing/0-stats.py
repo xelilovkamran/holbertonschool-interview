e50f0ce6b6521566f1d5c26b7458fa679e94664e
xelilovkamran <xlilovkamran@gmail.com>
2024-04-10 19:46:16 +0400

    log parse

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Reads stdin line by line and computes metrics
+""" Reads stdin line by line and computes metrics
 If count of lines is evenly divided by 10 and/or
 keyboardinterrupt
-all info will be printed"""
+all info will be printed """
 
 import sys
 
