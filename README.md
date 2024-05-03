f2e24826e17cbe30a274dbbd7c20501845169ab4
xelilovkamran <xlilovkamran@gmail.com>
2024-05-03 22:02:08 +0400

    UTF-8 Validation

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # holberton-interview
+
+### Requirements
+
+-   Allowed editors: vi, vim, emacs
+-   All your files will be interpreted/compiled on Ubuntu 14.04 LTS using python3 (version 3.4.3)
+-   All your files should end with a new line
+
+### Task
+
+Write a method that determines if a given data set represents a valid UTF-8 encoding.
+
+Prototype: `def validUTF8(data)`
+Return: `True` if data is a valid UTF-8 encoding, else return `False`
---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" UTF-8 Validation """
+
+
+def validUTF8(data):
+    """Function"""
+
+    count = 0
+    for dt in data:
+        binary = bin(dt[0]).replace('0b', '').rjust(8, '0')[::-1]
+        if count == 0:
+            if binary.startswith('110'):
+                count = 1
+            elif binary.startswith('1110'):
+                count = 2
+            elif binary.startswith('11110'):
+                count = 3
+            elif binary.startswith('10'):
+                return False
+        else:
+            if not binary.startswith("10"):
+                return False
+            count -= 1
+
+    return count == 0
