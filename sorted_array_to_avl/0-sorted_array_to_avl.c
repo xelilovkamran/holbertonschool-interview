53ce9d48ffbc275930dba783b7d49fe5bd31bd8c
xelilovkamran <xlilovkamran@gmail.com>
2024-06-07 16:42:47 +0400

    AVL - From sorted array

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,99 +1,43 @@
 #include "binary_trees.h"
-
 /**
- * sorted_array_to_avl - builds an AVL tree from sorted array without rotation
+ * ArrayToAVL - sort array
  *
- * @array: pointer to the first element of the array to be converted
- * @size: number of elements in the array
+ * @array: sorted array to be converted to a binary tree.
+ * @start: start node.
+ * @end: end node.
+ * @parent: parent node.
+ * Return: AVL tree
+ */
+
+avl_t *ArrayToAVL(int *array, int start, int end, avl_t *parent)
+{
+	avl_t *root;
+	int mid;
+
+	if (start > end)
+		return (NULL);
+	mid = (start + end) / 2;
+
+	root = malloc(sizeof(avl_t));
+	if (root == NULL)
+		return (NULL);
+	root->n = array[mid];
+	root->parent = parent;
+	root->left = ArrayToAVL(array, start, mid - 1, root);
+	root->right = ArrayToAVL(array, mid + 1, end, root);
+	return (root);
+
+}
+/**
+ * sorted_array_to_avl - Prints a binary tree
  *
- * Return: pointer to the root node of the created AVL tree, or NULL if failed
+ * @array: sorted array to be converted to a binary tree
+ * @size: size of the array
+ * Return: AVL tree
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-	avl_t *tree = NULL;
-	int check = 0;
-
-	if (array == NULL || size < 1)
+	if (array == NULL)
 		return (NULL);
-
-	check = add_node_avl(array, size, &tree, 1);
-	if (check)
-	{
-		free_avl(&tree);
-		return (NULL);
-	}
-
-	return (tree);
-}
-
-
-/**
- * add_node_avl - builds tree from sorted array, adding nodes recursively
- *
- * @array: pointer to the first element of the array to be converted
- * @size: number of elements in the array
- * @tree: double pointer to AVL tree
- * @add_left: flag to indicate if the new node will be a left child
- *
- * Return: 0 if successful, or -1 if failed
- */
-int add_node_avl(int *array, size_t size, avl_t **tree, int add_left)
-{
-	size_t halfway = (size - 1) / 2;
-	avl_t *new_node = NULL;
-	int check = 0;
-
-	if (array == NULL || size < 1)
-		return (0);
-
-	new_node = malloc(sizeof(avl_t));
-	if (new_node == NULL)
-		return (-1);
-	new_node->n = array[halfway];
-	new_node->parent = (*tree);
-	new_node->left = NULL;
-	new_node->right = NULL;
-
-	if ((*tree) == NULL)
-		(*tree) = new_node;
-	else if (add_left)
-		(*tree)->left = new_node;
-	else
-		(*tree)->right = new_node;
-
-	check = add_node_avl(array, halfway, &new_node, 1);
-	if (check == -1)
-		return (-1);
-
-	halfway++;
-	check = add_node_avl(&array[halfway], size - halfway, &new_node, 0);
-	if (check == -1)
-		return (-1);
-
-	return (0);
-}
-
-/**
- * free_avl - frees AVL tree if later node creation failed
- *
- * @tree: double pointer to AVL tree
- *
- */
-void free_avl(avl_t **tree)
-{
-	avl_t *left = NULL, *right = NULL;
-
-	if (tree == NULL || (*tree) == NULL)
-		return;
-
-	left = (*tree)->left;
-	right = (*tree)->right;
-
-	free((*tree)->parent);
-	free((*tree)->right);
-	free((*tree)->left);
-	free(*tree);
-
-	free_avl(&left);
-	free_avl(&right);
+	return (ArrayToAVL(array, 0, size - 1, NULL));
 }
--- sorted_array_to_avl/binary_trees.h
+++ sorted_array_to_avl/binary_trees.h
@@ -2,7 +2,8 @@
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -18,11 +19,9 @@ typedef struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 } binary_tree_t;
-
-void binary_tree_print(const binary_tree_t *);
-
 typedef struct binary_tree_s avl_t;
 
+void binary_tree_print(const binary_tree_t *);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 
-#endif /* _BINARY_TREES_H_ */
+#endif
