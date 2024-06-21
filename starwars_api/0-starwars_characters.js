a128b074a554843a43965d873152778906e474aa
xelilovkamran <xlilovkamran@gmail.com>
2024-06-21 18:40:03 +0400

    Star Wars Characters

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,21 +1,25 @@
 #!/usr/bin/node
 
-import request from "request";
+const request = require("request");
+const arg = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${arg}`;
 
-const movieID = process.argv[2];
+const charNames = (characters, i = 0) => {
+    if (i === characters.length) return;
+    request(characters[i], (error, response, body) => {
+        if (error) throw error;
+        // Convert a string of characters JSON to a JavaScript object and print it
+        console.log(JSON.parse(body).name);
+        // Call recursively the function charNames and increment it to pass to the next character
+        charNames(characters, i + 1);
+    });
+};
 
-request.get(
-    `https://swapi-api.hbtn.io/api/films/${movieID}`,
-    (error, response, body) => {
-        if (error) {
-            console.error(error);
-        } else {
-            const characters = JSON.parse(body).characters;
-            characters.forEach((character) => {
-                request.get(character, (error, response, body) => {
-                    console.log(JSON.parse(body).name);
-                });
-            });
-        }
-    }
-);
+// Request to the API to recover movie information
+request(url, function (error, response, body) {
+    if (error) throw error;
+    // Extract URL of characters from the body JSON response and store it in a var char
+    const char = JSON.parse(body).characters;
+    // Call the function charNames to print the characters
+    charNames(char);
+});
